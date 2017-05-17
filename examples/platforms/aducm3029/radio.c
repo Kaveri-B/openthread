9fdc0b191eb9680a6db565aaf762f3beb1f68c77
Kaveri <Kaveri.B@altiux.com>
2017-05-17 12:30:56 +0530

    Support for ADuCM3029 platform

---
+++ examples/platforms/aducm3029/radio.c
@@ -0,0 +1,271 @@
+/*
+ *  Copyright (c) 2016, The OpenThread Authors.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the copyright holder nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file
+ *   This file implements the OpenThread platform abstraction for radio communication.
+ *
+ */
+
+#include <openthread-config.h>
+
+#include "openthread/openthread.h"
+#include "openthread/platform/platform.h"
+#include "openthread/platform/radio.h"
+#include "openthread/platform/diag.h"
+
+#include "platform-aducm3029.h"
+
+
+
+
+void enableReceiver(void)
+{
+}
+
+void disableReceiver(void)
+{
+}
+
+void setChannel(uint8_t channel)
+{
+    (void)channel;
+}
+
+void otPlatRadioGetIeeeEui64(otInstance *aInstance, uint8_t *aIeeeEui64)
+{
+    (void)aInstance;
+    (void)aIeeeEui64;
+}
+
+void otPlatRadioSetPanId(otInstance *aInstance, uint16_t panid)
+{
+    (void)aInstance;
+    (void)panid;
+}
+
+void otPlatRadioSetExtendedAddress(otInstance *aInstance, uint8_t *address)
+{
+    (void)aInstance;
+    (void)address;
+}
+
+void otPlatRadioSetShortAddress(otInstance *aInstance, uint16_t address)
+{
+    (void)aInstance;
+    (void)address;
+}
+
+void aducm3029RadioInit(void)
+{
+}
+
+bool otPlatRadioIsEnabled(otInstance *aInstance)
+{
+    (void)aInstance;
+    return false;
+}
+
+ThreadError otPlatRadioEnable(otInstance *aInstance)
+{
+    (void)aInstance;
+
+    return kThreadError_NotImplemented;
+}
+
+ThreadError otPlatRadioDisable(otInstance *aInstance)
+{
+    (void)aInstance;
+
+    return kThreadError_NotImplemented;
+}
+
+ThreadError otPlatRadioSleep(otInstance *aInstance)
+{
+    (void)aInstance;
+
+    return kThreadError_NotImplemented;
+}
+
+ThreadError otPlatRadioReceive(otInstance *aInstance, uint8_t aChannel)
+{
+    (void)aInstance;
+    (void)aChannel;
+    return kThreadError_NotImplemented;
+}
+
+ThreadError otPlatRadioTransmit(otInstance *aInstance, RadioPacket *aPacket)
+{
+    (void)aInstance;
+    (void)aPacket;
+    return kThreadError_NotImplemented;
+}
+
+RadioPacket *otPlatRadioGetTransmitBuffer(otInstance *aInstance)
+{
+    (void)aInstance;
+    return NULL;
+}
+
+int8_t otPlatRadioGetRssi(otInstance *aInstance)
+{
+    (void)aInstance;
+    return 0;
+}
+
+otRadioCaps otPlatRadioGetCaps(otInstance *aInstance)
+{
+    (void)aInstance;
+    return kRadioCapsNone;
+}
+
+bool otPlatRadioGetPromiscuous(otInstance *aInstance)
+{
+    (void)aInstance;
+    return true;
+}
+
+void otPlatRadioSetPromiscuous(otInstance *aInstance, bool aEnable)
+{
+    (void)aInstance;
+    (void)aEnable;
+
+}
+
+void readFrame(void)
+{
+}
+
+void aducm3029RadioProcess(otInstance *aInstance)
+{
+    (void)aInstance;
+    readFrame();
+}
+
+
+uint32_t getSrcMatchEntriesEnableStatus(bool aShort)
+{
+    (void)aShort;
+    return 0;
+}
+
+int8_t findSrcMatchShortEntry(const uint16_t aShortAddress)
+{
+    (void)aShortAddress;
+
+    return -1;
+}
+
+int8_t findSrcMatchExtEntry(const uint8_t *aExtAddress)
+{
+    (void)aExtAddress;
+
+    return -1;
+}
+
+void setSrcMatchEntryEnableStatus(bool aShort, uint8_t aEntry, bool aEnable)
+{
+   (void)aShort;
+   (void)aEntry;
+   (void)aEnable;
+}
+
+int8_t findSrcMatchAvailEntry(bool aShort)
+{
+    (void)aShort;
+
+    return -1;
+}
+
+void otPlatRadioEnableSrcMatch(otInstance *aInstance, bool aEnable)
+{
+    (void)aInstance;
+    (void)aEnable;
+}
+
+ThreadError otPlatRadioAddSrcMatchShortEntry(otInstance *aInstance, const uint16_t aShortAddress)
+{
+    (void)aInstance;
+    (void)aShortAddress;
+    return kThreadError_NotImplemented;
+}
+
+ThreadError otPlatRadioAddSrcMatchExtEntry(otInstance *aInstance, const uint8_t *aExtAddress)
+{
+    (void)aInstance;
+    (void)aExtAddress;
+
+    return kThreadError_NotImplemented;
+}
+
+ThreadError otPlatRadioClearSrcMatchShortEntry(otInstance *aInstance, const uint16_t aShortAddress)
+{
+    (void)aInstance;
+    (void)aShortAddress;
+
+    return kThreadError_NotImplemented;
+}
+
+ThreadError otPlatRadioClearSrcMatchExtEntry(otInstance *aInstance, const uint8_t *aExtAddress)
+{
+    (void)aInstance;
+    (void)aExtAddress;
+
+    return kThreadError_NotImplemented;
+}
+
+void otPlatRadioClearSrcMatchShortEntries(otInstance *aInstance)
+{
+    (void)aInstance;
+}
+
+void otPlatRadioClearSrcMatchExtEntries(otInstance *aInstance)
+{
+    (void)aInstance;
+}
+
+ThreadError otPlatRadioEnergyScan(otInstance *aInstance, uint8_t aScanChannel, uint16_t aScanDuration)
+{
+    (void)aInstance;
+    (void)aScanChannel;
+    (void)aScanDuration;
+    return kThreadError_NotImplemented;
+}
+
+void otPlatRadioSetDefaultTxPower(otInstance *aInstance, int8_t aPower)
+{
+    (void)aInstance;
+    (void)aPower;
+}
+
+int8_t otPlatRadioGetReceiveSensitivity(otInstance *aInstance)
+{
+    (void)aInstance;
+    return 0;
+}
+
