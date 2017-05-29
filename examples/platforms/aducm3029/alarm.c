a11721514288ebeb5a919584b916ab51763cb8ad
reginalddsouza1 <reginald.d@altiux.com>
2017-05-29 18:14:59 +0530

    Support for platform Alarm

--- examples/platforms/aducm3029/alarm.c
+++ examples/platforms/aducm3029/alarm.c
@@ -41,32 +41,105 @@
 #include "openthread/platform/alarm.h"
 #include "openthread/platform/diag.h"
 #include "platform-aducm3029.h"
+#include "drivers/rtc/adi_rtc.h"
 
+#define RTC_DEV_NUM 1
 
+static ADI_RTC_HANDLE hDeviceRTC = NULL;
+static uint8_t rtc_dev_mem[ADI_RTC_MEMORY_SIZE];
+
+static uint32_t sCounter = 0;
+static uint32_t sAlarmT0 = 0;
+static uint32_t sAlarmDt = 0;
+static bool sIsRunning = false;
+
+static void rtc_callback(void *hWut, uint32_t event, void *pArgs)
+{
+	//adi_rtc_Enable(hDeviceRTC, false);
+	sCounter++;
+	adi_rtc_SetCount(hDeviceRTC, 0);
+	adi_rtc_SetAlarm(hDeviceRTC, 7);
+	adi_rtc_EnableAlarm(hDeviceRTC, true);
+	//adi_rtc_Enable(hDeviceRTC, true);
+}
 
 void aducm3029AlarmInit(void)
 {
+	ADI_RTC_INT_TYPE nInterrupts = ADI_RTC_ALARM_INT;
+	 
+	adi_rtc_Open(RTC_DEV_NUM, rtc_dev_mem, ADI_RTC_MEMORY_SIZE, &hDeviceRTC);
+	adi_rtc_RegisterCallback(hDeviceRTC, rtc_callback, hDeviceRTC);
+	adi_rtc_SetTrim(hDeviceRTC, ADI_RTC_TRIM_INTERVAL_15, ADI_RTC_TRIM_1, ADI_RTC_TRIM_SUB);
+	adi_rtc_EnableTrim(hDeviceRTC, true);
+	adi_rtc_EnableInterrupts(hDeviceRTC, nInterrupts, true);
+	adi_rtc_SetPreScale(hDeviceRTC, 2);
+	adi_rtc_SetCount(hDeviceRTC, 0);
+	adi_rtc_SetAlarm(hDeviceRTC, 7);
+	adi_rtc_EnableAlarm(hDeviceRTC, true);
+	adi_rtc_Enable(hDeviceRTC, true);
 }
 
 uint32_t otPlatAlarmGetNow(void)
 {
-    return 0;
+    return sCounter;
 }
 
 void otPlatAlarmStartAt(otInstance *aInstance, uint32_t t0, uint32_t dt)
 {
     (void)aInstance;
-    (void)t0;
-    (void)dt;
+    sAlarmT0 = t0;
+    sAlarmDt = dt;
+    sIsRunning = true;
 }
 
 void otPlatAlarmStop(otInstance *aInstance)
 {
     (void)aInstance;
+    adi_rtc_EnableAlarm(hDeviceRTC, false);
+	adi_rtc_Enable(hDeviceRTC, false);
+	sIsRunning = false;
 }
 
 void aducm3029AlarmProcess(otInstance *aInstance)
 {
-   (void)aInstance;
+    uint32_t expires;
+    bool fire = false;
+
+    if (sIsRunning)
+    {
+        expires = sAlarmT0 + sAlarmDt;
+
+        if (sAlarmT0 <= sCounter)
+        {
+            if (expires >= sAlarmT0 && expires <= sCounter)
+            {
+                fire = true;
+            }
+        }
+        else
+        {
+            if (expires >= sAlarmT0 || expires <= sCounter)
+            {
+                fire = true;
+            }
+        }
+
+        if (fire)
+        {
+            sIsRunning = false;
+
+#if OPENTHREAD_ENABLE_DIAG
+
+            if (otPlatDiagModeGet())
+            {
+                otPlatDiagAlarmFired(aInstance);
+            }
+            else
+#endif
+            {
+                otPlatAlarmFired(aInstance);
+            }
+        }
+    }
 }
 
