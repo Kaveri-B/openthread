e889f7d8e3fe6b0122b51dba0416723226107a32
Kaveri <Kaveri.B@altiux.com>
2017-06-05 09:35:10 +0530

    Support for Random number generator

--- examples/platforms/aducm3029/random.c
+++ examples/platforms/aducm3029/random.c
@@ -37,22 +37,68 @@
 #include "openthread/platform/random.h"
 
 #include "platform-aducm3029.h"
+#include "drivers/rng/adi_rng.h"
 
+/* RNG Device number */
+#define RNG_DEV_NUM              (0u)
+
+#define RNG_DEV_LEN_PRESCALER    (1u)
+
+#define RNG_DEV_LEN_RELOAD       (256u)
+
+/* RNG Device Handle */
+static ADI_RNG_HANDLE shRngDevice;
+/* Memory to handle CRC Device */
+static uint8_t sRngDevMem[ADI_RNG_MEMORY_SIZE] __attribute__((aligned(4)));
 
 void aducm3029RandomInit(void)
 {
-
+    adi_rng_Open(RNG_DEV_NUM, sRngDevMem, sizeof(sRngDevMem), &shRngDevice);
+    adi_rng_SetSampleLen(shRngDevice, RNG_DEV_LEN_PRESCALER, RNG_DEV_LEN_RELOAD);
 }
 
 uint32_t otPlatRandomGet(void)
 {
-    return 0;
+    bool bRNGRdy;
+    uint32_t nRandomNum;
+    uint8_t num_bytes = 0;
+    uint32_t random = 0;
+
+    adi_rng_Enable(shRngDevice, true);
+    
+    while ( num_bytes < 4)
+    {
+        adi_rng_GetRdyStatus(shRngDevice, &bRNGRdy);
+        if (bRNGRdy) 
+        {
+            adi_rng_GetRngData(shRngDevice, &nRandomNum);
+            random |= (nRandomNum & 0xFF) << (num_bytes * 8);
+            num_bytes++;
+        }
+    }    
+    adi_rng_Enable(shRngDevice, false);
+    
+    return random;
 }
 
 otError otPlatRandomGetTrue(uint8_t *aOutput, uint16_t aOutputLength)
 {
-    (void)aOutput;
-    (void)aOutputLength;
+    bool bRNGRdy;
+    uint32_t nRandomNum;
+    uint16_t num_bytes = 0;
+
+    adi_rng_Enable(shRngDevice, true);
+    
+    while ( num_bytes < aOutputLength)
+    {
+        adi_rng_GetRdyStatus(shRngDevice, &bRNGRdy);
+        if (bRNGRdy)
+        {
+            adi_rng_GetRngData(shRngDevice, &nRandomNum);
+            aOutput[num_bytes++] = nRandomNum & 0xFF;
+        }
+    }    
+    adi_rng_Enable(shRngDevice, false);
    
-    return OT_ERROR_NOT_IMPLEMENTED;
+    return OT_ERROR_NONE;
 }
