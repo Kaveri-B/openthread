8fd064d93329af1527b6a266269d328429ebaf65
Kaveri <Kaveri.B@altiux.com>
2017-08-14 12:12:17 +0530

    BSP for ADuCM3029 platform

---
+++ third_party/adi/ADuCM302x_IoT_DFP/1.0.0/Documents/html/navtreedata.js
@@ -0,0 +1,35 @@
+var NAVTREE =
+[
+  [ "ADuCM302x Device Drivers API Reference Manual", "index.html", [
+    [ "ADuCM302x_EZ-KIT Device Drivers API Reference Manual Overview", "index.html", [
+      [ "ADuCM302x Introduction", "index.html#ChipIntro", null ],
+      [ "API Reference Manual", "index.html#DriverIntro", null ],
+      [ "Getting Started Guide", "index.html#SettingStarted", null ],
+      [ "Release Notes", "index.html#ReleaseNotes", null ],
+      [ "Hardware User Guide and Device Data Sheet", "index.html#UserGuide", null ],
+      [ "Technical or Customer Support", "index.html#Support", null ]
+    ] ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"group___crypto___driver.html#gga9eabf513789131fcc3f194c2054dbca5ac21cdae867eee90b0d799880422335b4",
+"group___g_p_timer2___driver___config.html#gab646832dce50dc23380166876f6b524f",
+"group___r_n_g___driver___cfg.html#ga94539b647babe317770591e853347674",
+"group___s_p_i___driver.html#gaa320c1e6efc1046df04533082521e960",
+"group___t_m_r___driver.html#gga5964eb1c3bb3735e8a57b9527ee1b52aaa5b900299822ee51ed0ad9959ffa9f2d",
+"struct_a_d_i___c_r_y_p_t_o___t_r_a_n_s_a_c_t_i_o_n.html#a6b7509d85afd774a1f797728a16271fc"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
